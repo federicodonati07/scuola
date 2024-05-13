e95b9720b2b840f9f6dd54aea0cc044540e644e3
federicodonati07 <realeaquila.929@gmail.com>
2024-05-13 19:02:16 +0200

    setup

---
+++ app/(auth)/sing-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const singIn
+ = () => {
+  return (
+    <div>
+        singIn
+    </div>
+  )
+}
+
+export default singIn
---
+++ app/(auth)/sing-out/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const singOut
+ = () => {
+  return (
+    <div>
+        singOut
+    </div>
+  )
+}
+
+export default singOut
